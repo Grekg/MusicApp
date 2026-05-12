f39418f8f3bf5a1e42fccd9c13be274fca85c20d
Grekg <gregkar0570@gmail.com>
2026-05-12 12:38:55 +0300

    added network and thread dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,15 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging.interceptor)
+
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)

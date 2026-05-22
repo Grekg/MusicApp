da56a74579cd9f249ac2ff81a7b21ab17095ac27
mpopescu <156842677+MVPpopescu@users.noreply.github.com>
2026-05-22 17:58:28 +0300

    Built the activity_collection.xml layout

---
+++ app/src/main/java/gr/athenstech/musicapp/CollectionActivity.kt
@@ -0,0 +1,11 @@
+package gr.athenstech.musicapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CollectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_collection)
+    }
+}

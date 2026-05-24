dc122c78d2d9a2b92c48ccc7046da05dbb1a3081
mpopescu <156842677+MVPpopescu@users.noreply.github.com>
2026-05-24 13:24:54 +0300

    Created a database query that makes saved artist show on screen

--- app/src/main/java/gr/athenstech/musicapp/CollectionActivity.kt
+++ app/src/main/java/gr/athenstech/musicapp/CollectionActivity.kt
@@ -2,10 +2,34 @@ package gr.athenstech.musicapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import gr.athenstech.musicapp.ui.SavedArtistAdapter
+import kotlinx.coroutines.launch
 
 class CollectionActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: SavedArtistAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_collection)
+
+        recyclerView = findViewById(R.id.recycler_collection)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        adapter = SavedArtistAdapter(emptyList()) { }
+        recyclerView.adapter = adapter
+
+        val database = AppDatabase.getDatabase(this)
+        val artistDao = database.artistDao()
+
+        lifecycleScope.launch {
+            artistDao.getAllArtists().collect { artists ->
+                adapter.updateData(artists)
+            }
+        }
     }
 }
